9663e96559e1589454a093e2209e3f326d7585c3
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-24 23:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,72 +1,36 @@
 document.getElementById("btn").addEventListener("click", function (e) {
-  e.preventDefault();
-document.getElementById("btn").addEventListener("click", function (e) {
-  e.preventDefault();
+        e.preventDefault();
  
-  const ageInput = document.getElementById("age").value;
-  const nameInput = document.getElementById("name").value;
-  const age = Number(ageInput);
-  const name = nameInput.trim();
+        const ageInput = document.getElementById("age").value;
+        const nameInput = document.getElementById("name").value;
+        const age = Number(ageInput);
+        const name = nameInput.trim();
  
-  // Validation
-  if (!ageInput || !name) {
-    alert("Please enter valid details.");
-    return;
-  }
- 
-  // Promise Function
-  function checkEligibility(age, name) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (age > 18) {
-          resolve(`Welcome, ${name}. You can vote.`);
-        } else {
-          reject(`Oh sorry ${name}. You aren't old enough.`);
+        // Validation
+        if (!ageInput || !name) {
+          alert("Please enter valid details.");
+          return;
         }
-      }, 4000);
-    });
-  }
  
-  // Execute Promise
-  checkEligibility(age, name)
-    .then((message) => {
-      alert(message);
-    })
-    .catch((error) => {
-      alert(error);
-    });
-});
-  const ageInput = document.getElementById("age").value;
-  const nameInput = document.getElementById("name").value;
-
-  const age = Number(ageInput);
-  const name = nameInput.trim();
-
-  // Validation
-  if (!ageInput || !name) {
-    alert("Please enter valid details.");
-    return;
-  }
-
-  // Promise Function
-  function checkEligibility(age, name) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (age > 18) {
-          resolve(`Welcome, ${name}. You can vote.`);
-        } else {
-          reject(`Oh sorry ${name}. You aren't old enough.`);
+        // Promise Function
+        function checkEligibility(age, name) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              if (age > 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+              } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+              }
+            }, 4000);
+          });
         }
-      }, 4000);
-    });
-  }
-
-  // Execute Promise
-  checkEligibility(age, name)
-    .then((message) => {
-      alert(message);
-    })
-    .catch((error) => {
-      alert(error);
-    });
-});
+ 
+        // Execute Promise
+        checkEligibility(age, name)
+          .then((message) => {
+            alert(message);
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      });
