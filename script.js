1c2c6c4d723c5780dff7a87af5e7220e4dc72ddf
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-24 17:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+  // Validation
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Promise function
+  function checkEligibility(age, name) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age > 18) {
+          resolve(`Welcome, ${name}. You can vote.`);
+        } else {
+          reject(`Oh sorry ${name}. You aren't old enough.`);
+        }
+      }, 4000);
+    });
+  }
+
+  // Calling promise
+  checkEligibility(age, name)
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
